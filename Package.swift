1895fff16470d4c4a20ac63ca6f2ad5ab231dfd0
1Conan <me@1conan.com>
2022-10-03 10:32:33 +0800

    bump sdwebimage and librlottie

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-       .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-       .package(url: "https://github.com/SDWebImage/librlottie-Xcode.git", from: "0.2.0")
+       .package(url: "https://github.com/SDWebImage/SDWebImage.git", .branch("master")),
+       .package(url: "https://github.com/1Conan/librlottie-Xcode.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
